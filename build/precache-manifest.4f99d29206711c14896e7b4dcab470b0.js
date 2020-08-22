c9908dabafdbe286af52fd958c1e200e2ec90806
Wail Solaiman <wailsolaiman@hotmail.com>
2020-08-22 17:11:08 +0200

    added build folder

---
+++ build/precache-manifest.4f99d29206711c14896e7b4dcab470b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8523279d21ad4ba8420497c162d5d1cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1efedd30f3f228813999",
+    "url": "/static/css/main.c81e05bf.chunk.css"
+  },
+  {
+    "revision": "91d9f8d90835b91ca1d2",
+    "url": "/static/js/2.c9bc1330.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.c9bc1330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efedd30f3f228813999",
+    "url": "/static/js/main.09bede23.chunk.js"
+  },
+  {
+    "revision": "a51b60455636e3d1cbde",
+    "url": "/static/js/runtime-main.0e7eb838.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covid19.d7265326.png"
+  }
+]);
